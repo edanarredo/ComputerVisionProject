0b3b8113be4132bf39a5d886670909a2d122bc24
Daniel Arredondo <edanielarredondo@gmail.com>
2020-11-22 23:17:49 -0600

    Upload initial directories file

---
+++ directories.m
@@ -0,0 +1,4 @@
+% Replace template PATHS with local ones on your PC
+data_directory = 'fill in appropriately';
+code_directory = 'fill in appropriately';
+training_directory = 'fill in appropriately';
