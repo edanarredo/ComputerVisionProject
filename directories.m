9a5f2a5f41cca3575922729bd31b3ff55153fc84
Daniel Arredondo <edanielarredondo@gmail.com>
2020-11-30 17:32:26 -0600

    Small additions

--- directories.m
+++ directories.m
@@ -1,4 +1,4 @@
 % Replace template PATHS with local ones on your PC
 data_directory = 'C:\Users\Daniel\Desktop\ComputerVisionProject\data';
-code_directory = 'C:\Users\Daniel\Desktop\ComputerVisionProject\src';
+code_directory = 'C:\Users\Daniel\Desktop\ComputerVisionProject\test';
 training_directory = 'C:\Users\Daniel\Desktop\ComputerVisionProject\train';
