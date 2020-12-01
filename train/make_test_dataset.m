028432da5c2dd7973bb41214e9c1b054b21cd508
Daniel Arredondo <edanielarredondo@gmail.com>
2020-12-01 02:16:42 -0600

    progress on some files

--- train/make_test_dataset.m
+++ train/make_test_dataset.m
@@ -17,12 +17,28 @@ face_size = [100, 100];
 
 % load 100 faces
 % faces = a 3d array of 100 faces (size 100x100 by default)
+faces_list = dir('training_faces/*.bmp');
+face_names = {faces_list.name};
+
+faces = zeros(100, 100, 100);
+for i = 1: 100
+    faces(:,:,i) = imread(strcat('training_faces/',face_names{i}));
+end
 
 %% 
 
 % load 100 nonfaces
 % nonfaces = a 3d array of 100 nonfaces (resize to 100x100)
-% **the images need to be randomly cropped to 100x100 pixels of the nonface images**
+nonfaces_list = dir('training_nonfaces/*.jpg');
+nonface_names = {nonfaces_list.name};
+
+nonfaces = zeros(100, 100, 100);
+for i = 1: 100
+    nonface_to_insert = rgb2gray(imread(strcat('training_nonfaces/', nonface_names{i})));
+    imshow(nonface_to_insert, []);
+    [row, col] = size(nonface_to_insert);
+    nonface_to_insert = imcrop(nonface_to_insert, [row/2, col/2, row-101, col-101]);
+end
 
 %%
 
